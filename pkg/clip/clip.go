ac99329a984358d2373549260bc02bfcb9f2d0ec
luke lombardi <luke@slai.io>
2023-08-10 18:46:40 -0700

    enable debug

--- pkg/clip/clip.go
+++ pkg/clip/clip.go
@@ -171,6 +171,8 @@ func MountClipArchive(options MountOptions) (func() error, <-chan error, error) 
 			DisableXAttrs:        true,
 			EnableSymlinkCaching: true,
 			SyncRead:             false,
+			RememberInodes:       true,
+			Debug:                true,
 		},
 	}
 	server, err := fuse.NewServer(fs.NewNodeFS(root, fsOptions), options.MountPoint, &fuse.MountOptions{})
