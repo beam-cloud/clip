3f57451ad8b46bdcb70fe3377c928512c67364d2
luke lombardi <luke@slai.io>
2023-08-10 18:31:05 -0700

    Revert "try some profiling code"
    This reverts commit 35be44af84cb22906f74f3fc1bae337abd31791c.

--- pkg/clipfs/clipfs.go
+++ pkg/clipfs/clipfs.go
@@ -2,42 +2,22 @@ package clipfs
 
 import (
 	"fmt"
-	"log"
-	"time"
 
 	"github.com/beam-cloud/clip/pkg/common"
 	"github.com/beam-cloud/clip/pkg/storage"
 	"github.com/hanwen/go-fuse/v2/fs"
 )
 
-type ProfilingData struct {
-	Calls map[string]int
-	Times map[string]time.Duration
-}
-
 type ClipFileSystem struct {
-	s             storage.ClipStorageInterface
-	root          *FSNode
-	verbose       bool
-	profilingData *ProfilingData
-}
-
-func (cfs *ClipFileSystem) PrintProfilingData() {
-	for name, calls := range cfs.profilingData.Calls {
-		totalTime := cfs.profilingData.Times[name]
-		avgTime := totalTime / time.Duration(calls)
-		log.Printf("Profile %s: %d calls, total time: %v, average time per call: %v\n", name, calls, totalTime, avgTime)
-	}
+	s       storage.ClipStorageInterface
+	root    *FSNode
+	verbose bool
 }
 
 func NewFileSystem(s storage.ClipStorageInterface, verbose bool) (*ClipFileSystem, error) {
 	cfs := &ClipFileSystem{
 		s:       s,
 		verbose: verbose,
-		profilingData: &ProfilingData{
-			Calls: make(map[string]int),
-			Times: make(map[string]time.Duration),
-		},
 	}
 
 	metadata := s.Metadata()
@@ -50,18 +30,6 @@ func NewFileSystem(s storage.ClipStorageInterface, verbose bool) (*ClipFileSyste
 		filesystem: cfs,
 		attr:       rootNode.Attr,
 		clipNode:   rootNode,
-	}
-
-	if verbose {
-		go func() {
-			ticker := time.NewTicker(5 * time.Second)
-			defer ticker.Stop()
-
-			for {
-				<-ticker.C
-				cfs.PrintProfilingData()
-			}
-		}()
 	}
 
 	return cfs, nil
