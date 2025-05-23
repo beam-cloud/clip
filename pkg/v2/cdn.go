329ad5fe51a2a93b9d551ee9baea73088d02cf62
Daniel Levi-Minzi <dleviminzi@gmail.com>
2025-05-23 13:07:21 -0400

    cool down buffer sizes

--- pkg/v2/cdn.go
+++ pkg/v2/cdn.go
@@ -45,10 +45,10 @@ func init() {
 			MaxConnsPerHost:        100,
 			MaxIdleConns:           100,
 			MaxIdleConnsPerHost:    100,
-			ReadBufferSize:         2 * 1024 * 1024,
-			WriteBufferSize:        2 * 1024 * 1024,
+			ReadBufferSize:         512 * 1024,
+			WriteBufferSize:        512 * 1024,
 			DisableCompression:     true,
-			IdleConnTimeout:        90 * time.Second,
+			IdleConnTimeout:        5 * time.Second,
 			ResponseHeaderTimeout:  10 * time.Second,
 			ExpectContinueTimeout:  1 * time.Second,
 			TLSHandshakeTimeout:    10 * time.Second,
@@ -59,8 +59,7 @@ func init() {
 					KeepAlive: 30 * time.Second,
 					Control: func(network, address string, c syscall.RawConn) error {
 						return c.Control(func(fd uintptr) {
-							syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, syscall.SO_RCVBUF, 4*1024*1024)
-							syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, syscall.SO_SNDBUF, 4*1024*1024)
+							syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, syscall.SO_RCVBUF, 1024*1024)
 							syscall.SetsockoptInt(int(fd), syscall.IPPROTO_TCP, syscall.TCP_NODELAY, 1)
 						})
 					},
